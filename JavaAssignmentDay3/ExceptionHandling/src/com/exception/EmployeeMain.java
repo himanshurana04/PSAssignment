81b0e3f260002473e189f119deda105b40bdf6e7
himanshurana04 <58293409+himanshurana04@users.noreply.github.com>
2021-11-26 21:57:08 +0530

    Update EmployeeMain.java

--- JavaAssignmentDay3/ExceptionHandling/src/com/exception/EmployeeMain.java
+++ JavaAssignmentDay3/ExceptionHandling/src/com/exception/EmployeeMain.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 
 public class EmployeeMain {
 
-	public static void main(String[] args) throws NameException, AgeException {
+	public static void main(String[] args) //throws NameException, AgeException 
+	{
 		Employee emp=new Employee();
-		@SuppressWarnings("resource")
+		//@SuppressWarnings("resource")
+		try {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter no of emp you want ro add");
 		int noOfRecord=sc.nextInt();
 		for(int i=0;i<noOfRecord;i++) {
 			emp.addEmp();
+		}}
+		catch(AgeException a) {
+			a.printStackTrace();
 		}
-		
+		catch(NameException n) {
+			n.printStackTrace();
+		}
 		
 
 	}
