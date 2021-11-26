2a91cc6344d41c727c0336cfa3b67d32c94d1e0c
himanshurana04 <58293409+himanshurana04@users.noreply.github.com>
2021-11-26 08:26:24 +0530

    Add files via upload

---
+++ JavaAssignmentDay3/Sorting/src/ArraySort.java
@@ -0,0 +1,31 @@
+
+public class ArraySort {
+	void sort(int arr[])
+    {
+        int n = arr.length;
+ 
+        
+        for (int i = 0; i < n-1; i++)
+        {
+           
+            int min = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min])
+                    min = j;
+ 
+            
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+ 
+    
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+}
---
+++ JavaAssignmentDay3/Sorting/src/ArraySortMain.java
@@ -0,0 +1,11 @@
+
+public class ArraySortMain {
+	public static void main(String[] args) {
+ArraySort arr=new ArraySort();
+int [] arr2= {10,4,6,2,3,87,23};
+arr.printArray(arr2);
+arr.sort(arr2);
+arr.printArray(arr2);
+
+}
+}
